8b3ddff0a789490cafa7ac3bbb5a68892b3e02f8
Liz Baldo <lizbaldo@Lizs-MacBook-Pro.local>
2018-02-09 19:13:15 -0500

    [Problem 2 works!]

---
+++ problem_2/Fellow Codes Go Here/liz_baldo.py
@@ -0,0 +1,42 @@
+"""
+Given a binary tree, find its maximum depth
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node
+
+"""
+# Constructor to create a new node
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+
+def depth_helper(node):
+    if node == None:
+        return 0
+    else:
+        d_R = depth_helper(node.right)
+        d_L = depth_helper(node.left)
+        d = max(d_R,d_L)
+
+    return 1+d
+
+#PLEASE DO NOT CHANGE THIS
+def find_max_depth(tree):
+	depth = depth_helper(tree)
+	return depth
+
+
+
+#test cases
+def main():
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+    print ("Depth of tree is %d, and the expected result is 3" %(find_max_depth(root),))
+
+if __name__ == "__main__":
+    main()
